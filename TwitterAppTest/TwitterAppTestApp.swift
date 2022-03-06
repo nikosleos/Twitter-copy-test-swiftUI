dc0e61a7a6907d975ce8b799bb2df2df90d31680
n_leonov <65124365+n-leonov@users.noreply.github.com>
2022-03-06 20:19:07 +0300

    Initial Commit

---
+++ TwitterAppTest/TwitterAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterAppTestApp.swift
+//  TwitterAppTest
+//
+//  Created by Macbook on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
